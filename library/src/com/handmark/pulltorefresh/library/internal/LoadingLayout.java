17919b632f9e4be6cf861156909c0971479352b2
Edison Wang <ewang@squarespace.com>
2012-07-12 18:18:33 -0400

    minor fix

--- library/src/com/handmark/pulltorefresh/library/internal/LoadingLayout.java
+++ library/src/com/handmark/pulltorefresh/library/internal/LoadingLayout.java
@@ -77,19 +77,41 @@ public class LoadingLayout extends FrameLayout {
 		switch (mode) {
 			case PULL_UP_TO_REFRESH:
 				// Load in labels
-		
-				mPullLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_pull_label,R.string.pull_to_refresh_from_bottom_pull_label));
-				mRefreshingLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_refreshing_label, R.string.pull_to_refresh_from_bottom_refreshing_label));
-				mReleaseLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_release_label, R.string.pull_to_refresh_from_bottom_release_label));
+			  if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_pull_label)){
+			    mPullLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_pull_label);
+			  }else{
+          mPullLabel=context.getString(R.string.pull_to_refresh_from_bottom_pull_label);
+			  }
+        if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_refreshing_label)){
+          mRefreshingLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_refreshing_label);
+        }else{
+          mRefreshingLabel=context.getString(R.string.pull_to_refresh_from_bottom_refreshing_label);
+        }
+        if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_release_label)){
+          mReleaseLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_from_bottom_release_label);
+        }else{
+          mReleaseLabel=context.getString(R.string.pull_to_refresh_from_bottom_release_label);
+        }
 				break;
 
 			case PULL_DOWN_TO_REFRESH:
 			default:
 				// Load in labels
-			  mPullLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_pull_label,R.string.pull_to_refresh_pull_label_string));
-        mRefreshingLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_refreshing_label, R.string.pull_to_refresh_refreshing_label_string));
-        mReleaseLabel = context.getString(attrs.getInteger(R.styleable.PullToRefresh_pull_to_refresh_release_label, R.string.pull_to_refresh_release_label_string));
-				break;
+        if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_pull_label)){
+          mPullLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_pull_label);
+        }else{
+          mPullLabel=context.getString(R.string.pull_to_refresh_pull_label_string);
+        }
+        if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_refreshing_label)){
+          mRefreshingLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_refreshing_label);
+        }else{
+          mRefreshingLabel=context.getString(R.string.pull_to_refresh_refreshing_label_string);
+        }
+        if(attrs.hasValue(R.styleable.PullToRefresh_pull_to_refresh_release_label)){
+          mReleaseLabel=attrs.getString(R.styleable.PullToRefresh_pull_to_refresh_release_label);
+        }else{
+          mReleaseLabel=context.getString(R.string.pull_to_refresh_release_label_string);
+        } break;
 		}
 
 		if (attrs.hasValue(R.styleable.PullToRefresh_ptrHeaderTextColor)) {
